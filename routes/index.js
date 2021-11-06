882915aa47757691abd005358ec20dbb4090010e
Mark <mranda10@my.centennialcollege.ca>
2021-11-06 18:05:17 -0400

    Initial Commit Basic Setup and Home Page

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/home', (req, res, next) => {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/surveys', (req, res, next) => {
+  res.render('index', { title: 'Surveys' });
+});
+
+router.get('/coming-soon', (req, res, next) => {
+  res.render('index', {title: 'Coming Soon'})
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('index', { title: 'Contact' });
+});
+
+module.exports = router;
