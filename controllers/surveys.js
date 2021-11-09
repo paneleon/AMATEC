65397544018d666d6cdfc8ac78d8b7fff952ed72
paneleon <epanussy@my.centennialcollege.ca>
2021-11-09 13:05:34 -0500

    Creates displaySurveys in Surveys controller and modifies survey route

--- controllers/surveys.js
+++ controllers/surveys.js
@@ -4,3 +4,14 @@ let mongoose = require('mongoose');
 
 let db = require('../config/db');
 let Survey = require('../models/survey');
+
+module.exports.displaySurveys = (req, res, next) => {
+
+    Survey.find((error, surveyList) => {
+        if (error){
+            return console.log(error);
+        } else {
+            res.render("survey", {title: "Our Surveys", SurveyList: surveyList})
+        }
+    })
+}
--- routes/surveys.js
+++ routes/surveys.js
@@ -2,21 +2,12 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
+let surveysController = require('../controllers/surveys');
+
 // connect to our survey Model
 let Survey = require('../models/survey');
 
 /* GET for survey list - READ operation. */
-router.get('/', (req, res, next) => {
-
-    Survey.find((err, surveyList) => {
-        if (err) {
-            return console.error(err);
-        } else {
-            
-           res.render('survey', {title: 'Survey List', SurveyList : surveyList});
-        }
-    });
-
-});
+router.get('/', surveysController.displaySurveys);
 
 module.exports = router;
