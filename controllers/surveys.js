c69d873fe80154ed0950ad2e65aee5ea621cd5b7
Chitwan <chitwankatyal03@gmail.com>
2021-11-10 10:13:26 -0500

    added edit page

--- controllers/surveys.js
+++ controllers/surveys.js
@@ -3,6 +3,7 @@ let router = express.Router;
 let mongoose = require('mongoose');
 
 let db = require('../config/db');
+const survey = require('../models/survey');
 let Survey = require('../models/survey');
 
 module.exports.displaySurveys = (req, res, next) => {
@@ -15,3 +16,61 @@ module.exports.displaySurveys = (req, res, next) => {
         }
     })
 }
+
+//Displaying edit page
+module.exports.displayEditPage = (req,res,next) => {
+    let id = req.params.id;
+
+    Survey.findById(id, (err, surveyToEdit) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //show the edit view  
+            res.render('edit', {title: 'Edit Survey', surveys:surveyToEdit,
+        displayName: req.user ? req.user.displayName: ''});
+        }
+    });
+};
+
+//Processing Edit Page
+module.exports.processEditPage = (req,res,next) => {
+    let id = req.params.id;
+    let editedSurvey = Survey({
+        "_id": id,
+        "name": req.body.name,
+        "description": req.body.description
+    });
+    Survey.updateOne({_id: id}, editedSurvey, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            //refresh the survey list
+            res.redirect('/surveys');
+        }
+    });
+};
+
+//Deleting the Survey
+module.exports.performDelete = (req,res,next) => {
+    let id = req.params.id;
+
+    Survey.remove({_id: id}, (err) => {
+        if(err)
+        {
+            console.log(err);
+            res.end(err);
+        }
+        else
+        {
+            //refresh the survey list
+            res.redirect('/surveys');
+        }
+    });
+};
--- routes/surveys.js
+++ routes/surveys.js
@@ -10,4 +10,13 @@ let Survey = require('../models/survey');
 /* GET for survey list - READ operation. */
 router.get('/', surveysController.displaySurveys);
 
+/* GET Route for displaying the Edit Page - UPDATE Operation */
+router.get('/edit/:id', surveysController.displayEditPage);
+
+/* POST Route for processing the Edit page - UPDATE Operation */
+router.post('/edit/:id', surveysController.processEditPage);
+
+/* GET Route to perform Surveys DELETION - DELETE Operation */
+router.get('/delete/:id', surveysController.performDelete);
+
 module.exports = router;
