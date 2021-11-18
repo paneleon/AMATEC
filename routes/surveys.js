c572f1914329dc28c741381a6de534789a2d0a4e
Chitwan <chitwankatyal03@gmail.com>
2021-11-17 23:13:49 -0500

    Added authorization to survey list, edit, add and delete

--- routes/surveys.js
+++ routes/surveys.js
@@ -4,28 +4,39 @@ let mongoose = require('mongoose');
 
 let surveysController = require('../controllers/surveys');
 
+//helper function for guard purposes
+function requireAuth(req, res, next)
+{
+    //check if the user is logged in
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+
 // connect to our survey Model
 let Survey = require('../models/survey');
 
 /* GET for survey list - READ operation. */
-router.get('/', surveysController.displaySurveys);
+router.get('/', requireAuth, surveysController.displaySurveys);
 
 /* GET for survey list - READ operation. */
-router.get('/surveys', surveysController.displaySurveys);
+router.get('/surveys', requireAuth, surveysController.displaySurveys);
 
 /* GET for displaying the Add Page - CREATE OPERATION */
-router.get('/add', surveysController.displayAddPage);
+router.get('/add', requireAuth, surveysController.displayAddPage);
 
 /* POST for processing the Add Page */
-router.post('/add', surveysController.processAddPage);
+router.post('/add',  requireAuth, surveysController.processAddPage);
 
 /* GET for displaying the Edit Page - UPDATE OPERATION */
-router.get('/edit/:id', surveysController.displayEditPage);
+router.get('/edit/:id',  requireAuth, surveysController.displayEditPage);
 
 /* POST for processing the Edit Page */
-router.post('/edit/:id', surveysController.processEditPage);
+router.post('/edit/:id',  requireAuth, surveysController.processEditPage);
 
 /* GET for performing the Deletion - DELETE OPERATION */
-router.get('/delete/:id', surveysController.performDeletion);
+router.get('/delete/:id',  requireAuth, surveysController.performDeletion);
 
 module.exports = router;
